e16119af714497d05567188681dec1e400e2cb33
Jaehwi-So <sjh9708@gmail.com>
2024-03-25 20:20:47 +0900

    feat : AVL Tree

--- src/j05_tree/C03_AVLTree.java
+++ src/j05_tree/C03_AVLTree.java
@@ -1,19 +1,29 @@
 package j05_tree;
 
-import com.sun.jdi.Value;
-
 import java.util.LinkedList;
 import java.util.Queue;
 
-class BSTNode<Key extends Comparable<Key>, Value> {
+class AVLNode<Key extends Comparable<Key>, Value> {
     Key key;
     Value value;
-    BSTNode<Key, Value> left;
-    BSTNode<Key, Value> right;
+    AVLNode<Key, Value> left;
+    AVLNode<Key, Value> right;
 
-    public BSTNode(Key key, Value value){
+    int height;
+
+    public AVLNode(Key key, Value value){
         this.key = key;
         this.value = value;
+        this.height = 1;
+        this.left = this.right = null;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     public Key getKey() {
@@ -32,19 +42,19 @@ class BSTNode<Key extends Comparable<Key>, Value> {
         this.value = value;
     }
 
-    public BSTNode<Key, Value> getLeft() {
+    public AVLNode<Key, Value> getLeft() {
         return left;
     }
 
-    public void setLeft(BSTNode<Key, Value> left) {
+    public void setLeft(AVLNode<Key, Value> left) {
         this.left = left;
     }
 
-    public BSTNode<Key, Value> getRight() {
+    public AVLNode<Key, Value> getRight() {
         return right;
     }
 
-    public void setRight(BSTNode<Key, Value> right) {
+    public void setRight(AVLNode<Key, Value> right) {
         this.right = right;
     }
 
@@ -53,15 +63,15 @@ class BSTNode<Key extends Comparable<Key>, Value> {
 
 }
 
-class BinarySearchTree<Key extends Comparable<Key>, Value>{
-    BSTNode<Key, Value> root;
+class AVLTree<Key extends Comparable<Key>, Value>{
+    AVLNode<Key, Value> root;
 
     // 전위 순회
     public void preorder(){
         this.preorder(this.root);
     }
 
-    private void preorder(BSTNode<Key, Value> n){
+    private void preorder(AVLNode<Key, Value> n){
         if(n != null){
             System.out.print(n.key + " ");
             preorder(n.left);
@@ -75,7 +85,7 @@ class BinarySearchTree<Key extends Comparable<Key>, Value>{
         this.inorder(this.root);
     }
 
-    private void inorder(BSTNode<Key, Value> n){
+    private void inorder(AVLNode<Key, Value> n){
         if(n != null){
             inorder(n.left);
             System.out.print(n.key + " ");
@@ -88,7 +98,7 @@ class BinarySearchTree<Key extends Comparable<Key>, Value>{
         this.postorder(this.root);
     }
 
-    private void postorder(BSTNode<Key, Value> n){
+    private void postorder(AVLNode<Key, Value> n){
         if(n != null){
             postorder(n.left);
             postorder(n.right);
@@ -102,11 +112,11 @@ class BinarySearchTree<Key extends Comparable<Key>, Value>{
         this.levelorder(this.root);
     }
 
-    private void levelorder(BSTNode<Key, Value> n){
-        Queue queue = new LinkedList<BSTNode<Key, Value>>();
+    private void levelorder(AVLNode<Key, Value> n){
+        Queue queue = new LinkedList<AVLNode<Key, Value>>();
         queue.add(n);
         while(!queue.isEmpty()){
-            BSTNode<Key, Value> current = (BSTNode<Key, Value>) queue.poll();
+            AVLNode<Key, Value> current = (AVLNode<Key, Value>) queue.poll();
             System.out.print(current.key + " ");
             if(current.left != null){
                 queue.add(current.left);
@@ -122,7 +132,7 @@ class BinarySearchTree<Key extends Comparable<Key>, Value>{
         return this.getHeight(this.root);
     }
 
-    private int getHeight(BSTNode<Key, Value> n){
+    private int getHeight(AVLNode<Key, Value> n){
         if(n == null){
             return 0;
         }
@@ -135,7 +145,7 @@ class BinarySearchTree<Key extends Comparable<Key>, Value>{
         return this.getSize(this.root);
     }
 
-    private int getSize(BSTNode<Key, Value> root){
+    private int getSize(AVLNode<Key, Value> root){
         if(root == null){
             return 0;
         }
@@ -144,11 +154,11 @@ class BinarySearchTree<Key extends Comparable<Key>, Value>{
         }
     }
 
-    public boolean isEqual(BinarySearchTree<Key, Value> compare){
+    public boolean isEqual(AVLTree<Key, Value> compare){
         return this.isEqual(this.root, compare.root);
     }
 
-    private boolean isEqual(BSTNode<Key, Value> n, BSTNode<Key, Value> m){
+    private boolean isEqual(AVLNode<Key, Value> n, AVLNode<Key, Value> m){
         if(n == null || m == null){
             return n == m; //둘 다 null일 경우 True, 아니면 False(XOR)
         }
@@ -158,11 +168,11 @@ class BinarySearchTree<Key extends Comparable<Key>, Value>{
         return isEqual(n.left, m.left) && isEqual(n.right, m.right);
     }
 
-    public BSTNode search(Key k){
+    public AVLNode search(Key k){
         return search(k, this.root);
     }
 
-    private BSTNode search(Key k, BSTNode n){
+    private AVLNode search(Key k, AVLNode n){
         if(n == null){
             return null;
         }
@@ -183,51 +193,51 @@ class BinarySearchTree<Key extends Comparable<Key>, Value>{
         this.root = insert(k, v, root);
     }
 
-    private BSTNode insert(Key k, Value v, BSTNode n){
+    private AVLNode insert(Key k, Value v, AVLNode n){
         if(n == null){
-            return new BSTNode(k, v);
+            return new AVLNode(k, v);
         }
         int comp = n.getKey().compareTo(k);
 
-        // insert 재귀는 두 가지 케이스를 반환함.
-        // case1. 서브트리 탐색 끝에 Null일 경우에는 새로운 노드 반환
-        // case2. 서브트리 탐색 시 노드가 존재할 경우 기존 노드 반환
 
         if(comp == 0){  // 같은 키값의 노드 -> Value만 갱신(Key 중복 허용 X)
             n.setValue(v);
+            return n;
         }
-        else if(comp > 0){  // 현재 노드보다 키값이 작은 경우 -> case1) 왼쪽에 삽입 or case2) 기존 left 노드를 setLeft(변화 없음)
+        else if(comp > 0){  // 현재 노드보다 키값이 작은 경우
             n.setLeft(insert(k, v, n.getLeft()));
         }
-        else{ // 현재 노드보다 키값이 큰 경우 -> 오른쪽 서브트리에 삽입 -> case1) 오른쪽에 삽입 or case2) 기존 right 노드를 setRight(변화 없음)
+        else{ // 현재 노드보다 키값이 큰 경우 -> 오른쪽 서브트리에 삽입
             n.setRight(insert(k, v, n.getRight()));
         }
-        return n;
+
+        n.height = Math.max(height(n.left), height(n.right)) + 1;
+        return balance(n);
     }
 
 
-    public BSTNode getMin(){
+    public AVLNode getMin(){
         if(this.root == null){
             return null;
         }
         return getMin(this.root);
     }
 
-    private BSTNode getMin(BSTNode n){
+    private AVLNode getMin(AVLNode n){
         if(n.getLeft() == null){
             return n; //Left 존재하지 않으면 최소값
         }
         return getMin(n.getLeft()); //최소값 나올때까지 left로 이동하며 재귀호출
     }
 
-    public BSTNode getMax(){
+    public AVLNode getMax(){
         if(this.root == null){
             return null;
         }
         return getMax(this.root);
     }
 
-    private BSTNode getMax(BSTNode n){
+    private AVLNode getMax(AVLNode n){
         if(n.getRight() == null){
             return n;   //Right가 존재하지 않으면 최대값
         }
@@ -235,25 +245,23 @@ class BinarySearchTree<Key extends Comparable<Key>, Value>{
     }
 
     public void deleteMin(){
-        if(this.root == null){
-            System.out.println("Empty");
-        }
         this.root = deleteMin(this.root);
     }
 
-    private BSTNode deleteMin(BSTNode n){
+    private AVLNode deleteMin(AVLNode n){
         if(n.getLeft() == null){
             return n.getRight(); //Left 존재하지 않으면 최소값
         }
         n.setLeft(deleteMin(n.getLeft())); //최소값 나올 때 까지 Left로 이동
-        return n;
+        n.height = Math.max(height(n.left), height(n.right)) + 1;
+        return balance(n);
     }
 
     public void delete(Key k){
         this.root = delete(k, this.root);
     }
 
-    private BSTNode delete(Key k, BSTNode n){
+    private AVLNode delete(Key k, AVLNode n){
         if(n == null){
             return null;
         }
@@ -269,31 +277,84 @@ class BinarySearchTree<Key extends Comparable<Key>, Value>{
             if(n.getRight() == null){   //오른쪽 서브트리가 없는 경우 : 왼쪽 서브트리를 현재 노드의 위치로 연결
                 return n.getLeft();
             }
-            if(n.getLeft() == null){  //왼쪽 서브트리가 없는 경우 : 오른쪽 서브트리를 현재 노드의 위치로 연결
+            else if(n.getLeft() == null){  //왼쪽 서브트리가 없는 경우 : 오른쪽 서브트리를 현재 노드의 위치로 연결
                 return n.getRight();
             }
-            BSTNode targetNode = n; // 삭제 대상 노드를 targetNode로 설정
-            n = getMin(targetNode.getRight()); // 대체 : 삭제 대상의 오른쪽 서브트리의 최소값 노드를 삭제 위치로 이동
-            n.setRight(deleteMin(targetNode.getRight()));  // 대체 노드의 right : 삭제 대상의 right 서브트리로 설정 + 이동된 노드는 제거
-            n.setLeft(targetNode.getLeft());    // 대체 노드의 left : 삭제 대상의 left 서브트리로 설정
+            else{
+                AVLNode targetNode = n; // 삭제 대상 노드를 targetNode로 설정
+                n = getMin(targetNode.getRight()); // 대체 : 삭제 대상의 오른쪽 서브트리의 최소값 노드를 삭제 위치로 이동
+                n.setRight(deleteMin(targetNode.getRight()));  // 대체 노드의 right : 삭제 대상의 right 서브트리로 설정 + 이동된 노드는 제거
+                n.setLeft(targetNode.getLeft());    // 대체 노드의 left : 삭제 대상의 left 서브트리로 설정
+            }
+        }
+
+        n.height = Math.max(height(n.left), height(n.right)) + 1;
+        return balance(n);
+    }
+
+    //왼쪽 서브트리와 오른쪽 서브트리의 높이 차이 반환
+    private int bf(AVLNode n){
+        return height(n.left) - height(n.right);
+    }
+
+    private int height(AVLNode n){
+        if(n == null){
+            return 0;
+        }
+        return n.getHeight();
+    }
+
+
+    private AVLNode balance(AVLNode n){
+        //노드 N의 왼쪽 서브트리의 높이가 높음
+        if(bf(n) > 1){
+            //노드 N의 왼쪽 자식노드 -> 오른쪽 서브트리의 높이가 높음
+            if(bf(n.left) < 0){
+                n.left = rotateLeft(n.left); //LR유형(Left-Right Rotate)
+            }
+            n = rotateRight(n); //LL유형(Right Rotate) / LR유형(Left-Right Rotate)
+        }
+        //노드 N의 오른쪽 서브트리의 높이가 높음
+        else if(bf(n) < -1){
+            //노드 N의 오른쪽 자식노드 -> 왼쪽 서브트리의 높이가 높음
+            if(bf(n.right) > 0){
+                n.right = rotateRight(n.right);   //RL유형(Right-Left Rotate)
+            }
+            n = rotateLeft(n);  //RR유형(Left Rotate) / RL유형(Right-Left Rotate)
         }
         return n;
     }
+
+    // 왼쪽으로 회전하는 함수
+    private AVLNode<Key, Value> rotateLeft(AVLNode<Key, Value> n) {
+        AVLNode<Key, Value> pivot = n.right;    // 오른쪽 자식노드 X : 노드 N의 위치로 변경
+        n.right = pivot.left;   // 노드 X의 왼쪽 서브트리 : 노드 N의 오른쪽 서브트리로 위치 변경
+        pivot.left = n;     // 노드 N : 노드 X의 왼쪽 자식노드로 변경
+
+        // 높이 업데이트
+        n.height = Math.max(height(n.left), height(n.right)) + 1;
+        pivot.height = Math.max(height(pivot.left), height(pivot.right)) + 1;
+        return pivot;
+    }
+
+    // 오른쪽으로 회전하는 함수
+    private AVLNode<Key, Value> rotateRight(AVLNode<Key, Value> n) {
+        AVLNode<Key, Value> pivot = n.left; // 왼쪽 자식노드 X : 노드 N의 위치로 변경
+        n.left = pivot.right; // 노드 X의 오른쪽 서브트리 : 노드 N의 왼쪽 서브트리로 위치 변경
+        pivot.right = n; // 노드 N : 노드 X의 오른쪽 자식노드로 변경
+
+        // 높이 업데이트
+        n.height = Math.max(height(n.left), height(n.right)) + 1;
+        pivot.height = Math.max(height(pivot.left), height(pivot.right)) + 1;
+        return pivot;
+    }
 }
 
-public class C02_BinarySearchTree {
-    static BinarySearchTree makeBinarySearchTree(Integer lastKey){
+public class C03_AVLTree {
+    static AVLTree makeAVLTree(Integer lastKey){
 
 
-        BinarySearchTree<Integer, String> t = new BinarySearchTree<>();
-//        t.insert(600, "Frog");
-//        t.insert(400, "Dog");
-//        t.insert(300, "Carrot");
-//        t.insert(100, "Apple");
-//        t.insert(200, "Banana");
-//        t.insert(700, "Grape");
-//        t.insert(500, "Elephant");
-//        t.insert(lastKey, "Lemon");
+        AVLTree<Integer, String> t = new AVLTree<>();
         t.insert(100, "Apple");
         t.insert(200, "Banana");
         t.insert(300, "Carrot");
@@ -307,7 +368,7 @@ public class C02_BinarySearchTree {
     public static void main(String[] args) {
 
 
-        BinarySearchTree t = makeBinarySearchTree(800);
+        AVLTree t = makeAVLTree(800);
 
         System.out.print("Preorder : ");
         t.preorder();
@@ -329,10 +390,10 @@ public class C02_BinarySearchTree {
         System.out.println("Size : " + t.getSize());
 
 
-        BinarySearchTree t2 = makeBinarySearchTree(800);
+        AVLTree t2 = makeAVLTree(800);
         System.out.println("isEqual : " + t.isEqual(t2));
 
-        BinarySearchTree t3 = makeBinarySearchTree(700);
+        AVLTree t3 = makeAVLTree(700);
         System.out.println("isEqual : " + t.isEqual(t3));
 
         System.out.println("Search 300 : " + t.search(300).getValue());
@@ -341,7 +402,7 @@ public class C02_BinarySearchTree {
         System.out.println("Search getMax : " + t.getMax().getKey());
 
         System.out.println("Delete 400");
-        t.delete(100);
+        t.delete(400);
         System.out.print("Inorder : ");
         t.inorder();
         System.out.println();
